85f5473a4298f1dc4e73dce2f052f9afd7e7a057
Артем <cooler292@mail.ru>
2022-10-03 18:38:43 +0300

    Проверка

--- RevitEOM/EditCableStream/ViewModel/ViewModelEditCableStream.cs
+++ RevitEOM/EditCableStream/ViewModel/ViewModelEditCableStream.cs
@@ -131,7 +131,7 @@ namespace EditCableStream.ViewModel
                     }
                 }
             }
-
+            //провекра
             foreach (var CurFam in CurrentFamilys)
             {
                 Element elem = Doc.GetElement(CurFam);
